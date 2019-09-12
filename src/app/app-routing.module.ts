46c1d8afc63220a59b14fee46bf55bcde8eea9ed
difly <kryshen.yaroslav@gmail.com>
2019-09-12 20:07:52 +0300

    fix simple bug in app-routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { HeroesComponent } from '.heroes/heroes.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 const routes: Routes = [
   {
-    path: heroes, component: HeroesComponent
+    path: 'heroes', component: HeroesComponent
   }
 ]
 
@@ -15,8 +15,6 @@ const routes: Routes = [
   imports: [
     RouterModule.forRoot(routes)
   ],
-  export: [
-    RouterModule
-  ]
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
