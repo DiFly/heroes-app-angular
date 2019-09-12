419e62c86481ca3cd4ad91022d3fc566a57b1e8e
difly <kryshen.yaroslav@gmail.com>
2019-09-12 16:49:20 +0300

    Change app-routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HeroesComponent } from '.heroes/heroes.component';
+
+const routes: Routes = [
+  {
+    path: heroes, component: HeroesComponent
+  }
+]
 
 
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
+    RouterModule.forRoot(routes)
+  ],
+  export: [
+    RouterModule
   ]
 })
 export class AppRoutingModule { }
