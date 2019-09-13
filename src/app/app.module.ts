cc2617cba9703edb35e5907e4a0984f7b26138aa
difly <kryshen.yaroslav@gmail.com>
2019-09-13 23:09:04 +0300

    Change InMemoryDataService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,6 +26,10 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     FormsModule,
     AppRoutingModule,
     HttpClientModule,
+
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
     HttpClientInMemoryWebApiModule.forRoot(
       InMemoryDataService, { dataEncapsulation: false }
     )
