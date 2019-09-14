aaf42f4c399120db02b28abdfb50aba277126580
difly <kryshen.yaroslav@gmail.com>
2019-09-14 22:54:28 +0300

    Add new functionality - save

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -15,6 +15,10 @@ export class HeroService {
 
   private heroesUrl = 'api/heroes';
 
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
   constructor(
     private http: HttpClient,
     private messageSerice: MessageService
@@ -51,5 +55,12 @@ export class HeroService {
       catchError(this.handleError<Hero>(`getHero id=${id}`))
     );
     /* return of(HEROES.find(hero => hero.id === id)); */
+  }
+
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`update hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
   }
 }
